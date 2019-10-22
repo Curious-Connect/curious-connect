c7db75306ef73b79e8136fffab104c169fbac4ed
Tatijana Rajcic <tatijana.rajcic@essec.edu>
2019-10-23 01:02:04 +0200

    chqnges

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,51 +4,54 @@ import "../stylesheets/Nav.css";
 export default function Nav() {
   return (
     <nav>
-      <a className="logo" >
-        <img src="images/logo3.png" alt="Curious Connect Logo"/>
-      </a>
-      <div className="d-inline-block">
-          <button className="navbar-toggler hamburger hamburger-js hamburger--spring" type="button" data-toggle="collapse" data-target="#navbar_main" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="hamburger-box">
-                  <span className="hamburger-inner"></span>
-              </span>
-          </button>
-      </div>
+        <div className="logo" >
+            <img src="images/logo3.png" alt="Curious Connect Logo"/>
+        </div>
+        {/* <div className="hamburger">
+            <button className="navbar-toggler hamburger hamburger-js hamburger--spring" type="button" data-toggle="collapse" data-target="#navbar_main" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="hamburger-box">
+                    <span className="hamburger-inner"></span>
+                </span>
+            </button>
+        </div> */}
 
-      <div className="collapse navbar-collapse align-items-center justify-content-end" id="navbar_main">
-          <ul className="navbar-nav">
-              <li className="nav-item dropdown megamenu">
-                  <a className="nav-link" href="#adn">
-                      Notre ADN
-                  </a>
-              </li>
-              <li className="nav-item dropdown megamenu">
-                  <a className="nav-link" href="#appli">
-                      Ton App
-                  </a>
-              </li>
-              <li className="nav-item dropdown megamenu">
-                  <a className="nav-link" href="#démo">
-                      La Démo
-                  </a>
-              </li>
-              <li className="nav-item dropdown megamenu">
-                  <a className="nav-link" href="#team">
-                      La Dream Team
-                  </a>
-              </li>
-              <li className="nav-item dropdown megamenu">
-                  <a className="nav-link" href="#partenaires">
-                      Les Supers Curieux
-                  </a>
-              </li>
-              <li className="nav-item dropdown megamenu">
-                  <a className="nav-link" href="#contact">
-                      Connecte-moi !
-                  </a>
-              </li>
-          </ul>
-      </div>
+        <ul className="nav-list">
+            <li className="nav-item">
+                <a className="nav-link" href="#adn">
+                    Notre Mission
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#appli">
+                    L'App
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#communauté">
+                    La Communauté
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#team">
+                    La Dream Team
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#partenaires">
+                    Les Supers Curieux
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#recompenses">
+                    Nos Récompenses
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                    Rejoignez-nous !
+                </a>
+            </li>
+        </ul>
     </nav>
   )
 }
